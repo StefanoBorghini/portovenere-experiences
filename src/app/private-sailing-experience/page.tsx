4466a5524613ec4ec201773d44c48c294372fd37
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-05-12 08:59:52 +0200

    nuove modifiche

---
+++ src/app/private-sailing-experience/page.tsx
@@ -0,0 +1,9 @@
+export default function PrivateSailingPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-6xl">
+        Private Sailing Experience
+      </h1>
+    </main>
+  );
+}
