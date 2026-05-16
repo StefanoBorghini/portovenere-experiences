d3a62950f94e404a0df71c458e0d09eaac13d288
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-05-16 11:58:50 +0200

    nuove modifiche

--- src/app/craft-your-experience/page.tsx
+++ src/app/craft-your-experience/page.tsx
@@ -429,9 +429,9 @@ traveling_with_children:
 
         {/* TOP */}
 
-<div className="text-center mb-20 pt-4 md:pt-8">
+<div className="text-center mb-20 -mt-10 md:-mt-16">
 
-  <div className="flex justify-center mb-5">
+  <div className="flex justify-center mb-4">
     <img
       src="/logo-white.png"
       alt="Portovenere Experiences"
