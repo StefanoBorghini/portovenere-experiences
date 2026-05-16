74e0c71b3e56e33304a211eff4931b42935a3960
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-05-16 20:45:02 +0200

    nuove modifiche

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,9 @@ import type { NextRequest } from "next/server";
 
 const protectedRoutes: Record<string, string> = {
   "/private-sailing-experience-with-snorkeling": "sailing2026@",
+    username: "Carolina",
+    password: "sailing2026@",
+    expiresAt: "2026-05-17T23:59:59",
 };
 
 export function middleware(request: NextRequest) {
@@ -26,7 +29,16 @@ export function middleware(request: NextRequest) {
 
       const response = NextResponse.next();
 
-      response.cookies.set("clientName", user);
+response.cookies.set(
+  "clientName",
+  user,
+  {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    maxAge: 60 * 60 * 24,
+  }
+);      
 
       return response;
     }
