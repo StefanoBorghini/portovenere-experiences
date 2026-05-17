76ff15d785bb91858efe1bc6de1603b3576584f8
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-05-17 17:22:42 +0200

    nuove modifiche

--- src/app/bachelorette-party-boat-lucia/page.tsx
+++ src/app/bachelorette-party-boat-lucia/page.tsx
@@ -27,7 +27,10 @@ export default async function ProposalPage() {
       "Private Boat Experience",
 
     description:
-      "5H Tour: Embarkation / Disembarkation at Porto Mirabello (La Spezia): transit in the Portovenere canal with a view of San Pietro and Palmaria island. Tour of the Cinque Terre Marine Protected Area with a view of the villages from the sea with a stop in front of each of them with a description of the main farms belonging to the area and tasting of local food and wine products.",
+    "5H Tour: Embarkation / Disembarkation at Porto Mirabello (La Spezia). " +
+    "Transit in the Portovenere canal with a view of San Pietro and Palmaria island. " +
+    "Tour of the Cinque Terre Marine Protected Area with a view of the villages from the sea." +
+    "with a stop in front of each of them with a description of the main farms belonging to the area and tasting of local food and wine products.",
   };
 
   const galleryImages = [
