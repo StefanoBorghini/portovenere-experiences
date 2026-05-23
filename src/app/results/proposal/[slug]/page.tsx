96b5f5f79cc0e8928e969f65bfdacc545a451df0
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-05-23 13:49:29 +0200

    nuove modifiche

--- src/app/results/proposal/[slug]/page.tsx
+++ src/app/results/proposal/[slug]/page.tsx
@@ -53,25 +53,23 @@ export default async function ProposalPage({
   // GENERATE PROPOSAL
 
   const generatedProposal =
-    generateProposal({
+  generateProposal({
 
-      experiencesSelected:
-        lead.experiences || [],
+    experiencesSelected:
+      lead.experiences || [],
 
-      moodsSelected:
-        lead.moods || [],
+    moodsSelected:
+      lead.moods || [],
 
-      budget:
-        lead.budget,
+    budget:
+      lead.budget,
 
-  guests:
+    guests:
       lead.guests,
 
     travelingWithChildren:
       lead.traveling_with_children || false,
-
-        
-    });
+  });
 
     const includedSections =
   generatedProposal.includedSections;
