90262119880ac27f4710eed832c2e0806646cda2
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-05-24 14:31:16 +0200

    nuove modifiche

--- src/lib/experienceConflicts.ts
+++ src/lib/experienceConflicts.ts
@@ -4,34 +4,112 @@ export const experienceConflicts: Record<
 > = {
 
   snorkeling: [
-    "mermaiding",
-  ],
+
+  "mermaiding",
+
+  "horses",
+
+  "airplane",
+
+  "mongolfiera",
+],
+
+
+
 
   mermaiding: [
     "snorkeling",
+    "horses",
+
+  "airplane",
+
+  "mongolfiera",
   ],
 
+
+
+
+
   foodwine: [
-    "restaurant",
-  ],
+
+  "restaurant",
+
+  "horses",
+
+  "airplane",
+
+  "mongolfiera",
+],
+
+
+
 
   restaurant: [
     "foodwine",
   ],
 
+
+
+
   sailing: [
     "motorboat",
+
+    "airplane",
+
+  "mongolfiera",
+
+   "horses",
   ],
+
+
 
   motorboat: [
     "sailing",
+
+    "airplane",
+
+  "mongolfiera",
+
+   "horses",
   ],
+
+
 
   trekking: [
-    "horsers",
+    "horses",
   ],
 
-  horsers: [
+
+
+
+  horses: [
     "trekking",
+     "mermaiding",
+"sailing",
+
+     "motorboat",
+  "snorkeling",
+
+ 
   ],
+
+
+
+
+  mongolfiera: [
+
+  "mermaiding",
+
+  "snorkeling",
+
+  "foodwine",
+
+  "airplane",
+
+    "motorboat",
+
+     "sailing",
+
+
+],
 };
